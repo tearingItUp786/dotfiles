18d5b209264d6bcf52831ebef4c4333df768620b
Taranveer (Taran) Bains <taranveer786@gmail.com>
2024-09-23 09:11:23 -0700

    chore: update copilot

--- nvim-work/lua/tearingitup786/plugins/copilot.lua
+++ nvim-work/lua/tearingitup786/plugins/copilot.lua
@@ -1,49 +1,56 @@
-local status, copilot = pcall(require, "copilot")
-if not status then
-	print("copilot not installed")
-	return
-end
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		config = function()
+			local status, copilot = pcall(require, "copilot")
+			if not status then
+				print("copilot not installed")
+				return
+			end
 
-copilot.setup({
-	panel = {
-		enabled = true,
-		auto_refresh = false,
-		keymap = {
-			jump_prev = "[[",
-			jump_next = "]]",
-			accept = "<CR>",
-			refresh = "gr",
-			open = "<M-CR>",
-		},
-		layout = {
-			position = "bottom", -- | top | left | right
-			ratio = 0.4,
-		},
-	},
-	suggestion = {
-		enabled = true,
-		auto_trigger = false,
-		debounce = 75,
-		keymap = {
-			accept = "<M-l>",
-			accept_word = false,
-			accept_line = false,
-			next = "<M-]>",
-			prev = "<M-[>",
-			dismiss = "<C-]>",
-		},
-	},
-	filetypes = {
-		yaml = false,
-		markdown = false,
-		help = false,
-		gitcommit = false,
-		gitrebase = false,
-		hgcommit = false,
-		svn = false,
-		cvs = false,
-		["."] = false,
-	},
-	copilot_node_command = "node", -- Node.js version must be > 16.x
-	server_opts_overrides = {},
-})
+			copilot.setup({
+				panel = {
+					enabled = true,
+					auto_refresh = false,
+					keymap = {
+						jump_prev = "[[",
+						jump_next = "]]",
+						accept = "<CR>",
+						refresh = "gr",
+						open = "<M-CR>",
+					},
+					layout = {
+						position = "bottom", -- | top | left | right
+						ratio = 0.4,
+					},
+				},
+				suggestion = {
+					enabled = true,
+					auto_trigger = false,
+					debounce = 75,
+					keymap = {
+						accept = "<M-l>",
+						accept_word = false,
+						accept_line = false,
+						next = "<M-]>",
+						prev = "<M-[>",
+						dismiss = "<C-]>",
+					},
+				},
+				filetypes = {
+					yaml = false,
+					markdown = false,
+					help = false,
+					gitcommit = false,
+					gitrebase = false,
+					hgcommit = false,
+					svn = false,
+					cvs = false,
+					["."] = false,
+				},
+				copilot_node_command = "node", -- Node.js version must be > 16.x
+				server_opts_overrides = {},
+			})
+		end,
+	}, -- or github/copilot.vim
+}
