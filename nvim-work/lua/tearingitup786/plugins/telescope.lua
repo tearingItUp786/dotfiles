9eb2ba388061f0054114faf4b65b01396fb7a459
Taranveer Bains <taranveer786@gmailcom>
2024-10-22 11:25:20 -0700

    chore: update telescope to allow us to delete buffers

--- nvim-work/lua/tearingitup786/plugins/telescope.lua
+++ nvim-work/lua/tearingitup786/plugins/telescope.lua
@@ -8,12 +8,16 @@ return {
 		telescope.setup({
 			defaults = {
 				mappings = {
+					n = {
+						["<C-d>"] = actions.delete_buffer,
+					},
 					i = {
 						["<C-n>"] = actions.cycle_history_next,
 						["<C-p>"] = actions.cycle_history_prev,
 						["<C-j>"] = actions.move_selection_next,
 						["<C-k>"] = actions.move_selection_previous,
 						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+						["<C-d>"] = actions.delete_buffer,
 					},
 				},
 			},
