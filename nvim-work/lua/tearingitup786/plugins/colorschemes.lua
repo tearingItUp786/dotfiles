71abd932d64eb5c8488895b04376f0471b53b215
Taranveer (Taran) Bains <taranveer786@gmail.com>
2024-10-30 09:38:20 -0700

    chore: minor updates nvim-work for trouble.lua

--- nvim-work/lua/tearingitup786/plugins/colorschemes.lua
+++ nvim-work/lua/tearingitup786/plugins/colorschemes.lua
@@ -1,17 +1,25 @@
 return {
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+	-- 			transparent = true,
+	-- 			styles = {
+	-- 				sidebars = "transparent",
+	-- 				floats = "transparent",
+	-- 			},
+	-- 		})
+	--
+	-- 		vim.cmd([[colorscheme tokyonight]])
+	-- 	end,
+	-- },
 	{
-		"folke/tokyonight.nvim",
-		config = function()
-			require("tokyonight").setup({
-				style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-				transparent = true,
-				styles = {
-					sidebars = "transparent",
-					floats = "transparent",
-				},
-			})
 
-			vim.cmd([[colorscheme tokyonight]])
+		"nyoom-engineering/oxocarbon.nvim",
+		config = function()
+			vim.opt.background = "dark" -- set this to dark or light
+			vim.cmd("colorscheme oxocarbon")
 		end,
 	},
 }
