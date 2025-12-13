dd3bf28f4f034993e4d14504672982946a9797d3
Taranveer Bains <taranveer786@gmailcom>
2025-12-13 15:54:33 -0800

    chore: get rid of supermaven

--- nvim-home/lua/custom/completion.lua
+++ nvim-home/lua/custom/completion.lua
@@ -40,25 +40,6 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		-- Add Tab to jump through snippet nodes
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources(preferred_sources, {
 		{ name = "buffer" },
