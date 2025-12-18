16ea23a305a3593612a0deb62ff4fb7ffc7a7e42
Taranveer Bains <taranveer786@gmailcom>
2025-12-17 20:33:13 -0800

    feat: migrate to snacks over telescope, nvim-tree, and nescroll.

--- nvim-home/lua/tearingitup786/plugins/dashboard.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{
-		"glepnir/dashboard-nvim",
-		dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	},
-	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" }, -- for markdown previewing
-}
