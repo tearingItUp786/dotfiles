769662bafdb1f869cc16c0cf3b845eaceb568185
Taranveer (Taran) Bains <taranveer786@gmail.com>
2025-12-18 10:59:55 -0800

    chore: add in imagemagick

--- nvim-home/lua/tearingitup786/plugins/snacks.lua
+++ nvim-home/lua/tearingitup786/plugins/snacks.lua
@@ -34,7 +34,15 @@ return {
 		quickfile = { enabled = true },
 		scope = { enabled = true },
 		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
+		statuscolumn = {
+			enabled = true,
+			left = { "mark", "sign" },
+			right = { "fold", "git" },
+			folds = {
+				open = true,
+				git_hl = false,
+			},
+		},
 		words = { enabled = true },
 	},
 	keys = {
